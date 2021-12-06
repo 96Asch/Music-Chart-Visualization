cb40229e5fa5e2adb9431f62d581b52dd33d73a0
96Asch <aschhuang@gmail.com>
2021-12-06 17:52:59 +0100

    Include slider indices into plot

---
+++ javascript/slider.js
@@ -0,0 +1,32 @@
+const sliderWidth = 700;
+const sliderCallbacks = [draw, draw_duplicates_plot];
+
+console.log(draw_duplicates_plot);
+function create_slider(data) {
+
+    const simpleSlider = d3.sliderBottom()
+    .min(0).max(42).step(1)
+    .width(sliderWidth - 100)
+    .displayValue(false)
+    .on('onchange', (val) => {
+        for (const cb of sliderCallbacks) cb(val);
+    });
+
+    const slider = d3.select('#slider')
+    .append('svg')
+    .attr('width', sliderWidth)
+    .attr('height', 90)
+    .append('g')
+    .attr('transform', 'translate(42,30)')
+    .attr('id', "somebs")
+    .call(simpleSlider);
+
+        simpleSlider.max(data["weeks"].length - 2)
+            .tickFormat((i) => data["weeks"][i].week.substring(0, 4));
+            
+        const ssvg = d3.select('#slider > svg > g');
+        ssvg.selectAll("*").remove();
+        ssvg.call(simpleSlider);
+        simpleSlider.value(Math.floor(data["weeks"].length / 2));
+    return slider;
+}
