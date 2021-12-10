0fe6b378f628b2ad960ac178989cef5df367152c
DaanG <d.h.e.groenewegen@student.tudelft.nl>
2021-12-10 16:57:41 +0100

    Working images in bubbles

--- javascript/bubbles.js
+++ javascript/bubbles.js
@@ -17,17 +17,37 @@ const colorset = {
     'R&B and Soul': "#37c7d7",
     'Rock': "#9b959a"
 }
-
 function init_bubbles() {
     // NOTE this function is called before the data is there.
     // Thus don't use the 'data' variable in this function!
     // Initialize all circles
-    var node = d3.select("#bubbles").append("g")
-        .selectAll("circle").data(Object.keys(colorset))
-        .enter().append("circle")
+    var svg = d3.select("#bubbles")
+    var defs = svg.append("defs");
+    var pattern = defs.selectAll("circle")
+        .data(Object.keys(colorset))
+        .enter().append("pattern")
         .style("stroke", "gray")
-        .style("fill", (d, i) => colorset[d])
-        .attr("r", (genre) => 0);
+        .attr("x", "0%")
+        .attr("y", "0%")
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr("viewBox", "0 0 512 512")
+        .attr("id", d => "circle" + d.toLowerCase().replace(/ /g,'').replace("&", ""))
+        .append("svg:image")
+        .attr("x", "0%")
+        .attr("y", "0%")
+        .attr("width", 512)
+        .attr("height", 512)
+        .attr("xlink:href", d => "img/" + d.toLowerCase().replace(/ /g,'').replace("&", "") + ".png")
+        
+    for (key in Object.keys(colorset)) {
+        let genre = Object.keys(colorset)[key];
+        genre = genre.toLowerCase().replace(/ /g,'').replace("&", "")
+        var circles = svg
+            .append("circle")
+            .attr("r", 0)
+            .style("fill", "green")
+    }
 }
 
 function draw_bubbles(week_index) {
@@ -53,7 +73,6 @@ function draw_bubbles(week_index) {
     var weekData = transformDataToGroup(about.popularity);
     var values = weekData.map(a => a.value);
     var sumPopularity = values.reduce((a,b) => a + b);
-
     var root = {
         "name" : "root",
         "children" : weekData
@@ -62,16 +81,20 @@ function draw_bubbles(week_index) {
         .sum(d => d.value)
         .sort((a,b) => b.value - a.value)
     var nodes = pack(root)
-    var node = d3.select("#bubbles").selectAll("circle")
+    var circles = d3.select("#bubbles").selectAll("circle")
         .data(nodes)
-        .style("fill-opacity", d => d.data.value * 0.05)
+        .style("fill", function(d) {
+            let name  = d.data.name;
+            name = name.toLowerCase().replace(/ /g,'').replace("&", "");
+            return "url(#circle" + name + ")";
+        })
         .attr("r", d => getRadius(d, sumPopularity, maxHeight))
         .attr("cx", d => d.x)
         .attr("cy", d => d.y)
         .on("mouseover", function(event, d) {
             d3.select(this).transition()
                 .duration("100")
-                .attr("r", d => getRadius(d, sumPopularity, maxHeight) + 5)
+                .attr("r", d => getRadius(d, sumPopularity, maxHeight) + 25)
         })
         .on("mouseout", function(event, d) {
             d3.select(this).transition()
@@ -87,7 +110,6 @@ function draw_bubbles(week_index) {
             //     }
             // }
         });
-
 }
 
 
@@ -101,11 +123,14 @@ function getRadius(d, sum, maxHeight) {
 function transformDataToGroup(data) {
     let result = [];
     for (var key in data) {
-        let entry = {
-            "name" : key,
-            "value" : data[key],
-        }
-        result.push(entry);
+        // Filter out unused genres for easier packing
+        if (data[key] > 0) {
+            let entry = {
+                "name" : key,
+                "value" : data[key],
+            }
+            result.push(entry)
+        };
     }
     return result;
 }
